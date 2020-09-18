38370f75983ab3cad7176f12c05169b937e9edf4
omerdlv <omerr118@gmail.com>
2020-09-18 14:30:33 +0300

    adding tests, targets, and updates

--- pylb/main.py
+++ pylb/main.py
@@ -2,25 +2,48 @@
 import requests
 from .vars import *
 from pylb.targets.targets import *
-from flask import Flask
-app = Flask(__name__)
+import flask
+import multiprocessing
+app = flask.Flask(__name__)
+ipc_manager = multiprocessing.Manager()
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+
+    if flask.request.method == "GET":
+        response = handle_get_request(flask.request)
+    elif flask.request.method == "POST":
+        response = handle_post_request(flask.request)
+    else:
+        response = flask.Response(status="LB does not support the request method")
+        response.status_code = 400
+
+    return response
 
 
-def handle_get_request(request: requests.Request) -> requests.Response:
-    return requests.get("")
+def handle_get_request(request: flask.Request) -> flask.Response:
+    try:
+        response = requests.get(url=request.url, headers=request.headers)
+    except RuntimeError as e:
+        terminate(ec_runtime_error)
+    finally:
+        terminate(ec_general_error)
+    return
 
 
-def handle_post_request(request: requests.Request) -> requests.Response:
+def handle_post_request(request: flask.Request) -> flask.Response:
     return requests.post("")
 
 
 def prepare_env():
     targets_pool = populate_target_list_from_json(targets_json_file)
+    healthy_targets = ipc_manager.list()
+
+
+def terminate(exit_code: int):
+    print("{}\n".format(errors_dict[exit_code]))
+    exit(exit_code)
 
 
 if __name__ == '__main__':
--- pylb/vars.py
+++ pylb/vars.py
@@ -2,3 +2,14 @@
 targets_pool = []
 healthy_targets = []
 targets_json_file = "../data/init_targets.json"
+
+# error and exception handling
+ec_success = 0
+ec_runtime_error = 1
+ec_general_error = 2
+
+errors_dict = {
+    ec_success: "execution finished successfully!",
+    ec_runtime_error: "runtime exception occured, exiting...",
+    ec_general_error: "general error occured, exiting..."
+}
